aac9056b08679d284d3a12a4e262ee55823c17f3
jemmyhuang <jemmy_huang@163.com>
2018-01-20 18:58:01 +0800

    对日志进行配置处理

--- src/main/java/com/example/exam/controller/LoginController.java
+++ src/main/java/com/example/exam/controller/LoginController.java
@@ -6,6 +6,8 @@ import com.example.exam.service.UserService;
 import io.swagger.annotations.ApiImplicitParam;
 import io.swagger.annotations.ApiImplicitParams;
 import io.swagger.annotations.ApiOperation;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,17 +18,24 @@ import javax.annotation.Resource;
 @RequestMapping("/main")
 public class LoginController {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(LoginController.class);
+
     @Resource(name = "userService")
     UserService userService;
 
     @ApiOperation(value = "登陆", notes = "根据username和password登陆用户")
     @ApiImplicitParams({
-            @ApiImplicitParam(name = "username", value = "用户名", required = true, paramType = "query", dataType = "String"),
-            @ApiImplicitParam(name = "password", value = "密码", required = true, paramType = "query", dataType = "String"),
+            @ApiImplicitParam(name = "username", value = "用户名", required = true, paramType = "query", dataType =
+                    "String"),
+            @ApiImplicitParam(name = "password", value = "密码", required = true, paramType = "query", dataType =
+                    "String"),
     })
     @PostMapping(value = "/login", produces = "application/json; charset=UTF-8")
     public ModelResult login(String username, String password) {
+        LOGGER.info("username:{},password:{}", username, password);
+        LOGGER.warn("username:{},password:{}", username, password);
+        LOGGER.error("username:{},password:{}", username, password);
         User user = userService.queryUserByName(username, password);
-        return new ModelResult<User>(user);
+        return new ModelResult<>(user);
     }
 }
